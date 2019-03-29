ae9a96091bd88d958c9730e95541f35a6fc1739d
Diego Gomez <df.gomez11@uniandes.edu.co>
2019-03-29 11:31:41 -0500

    Update train.py

--- train.py
+++ train.py
@@ -1,39 +1,3 @@
-# from system import System
-# pendulum = System()
-# tr_epsds = 125
-# epsd_steps = 800
-# mean_rewards = []
-# for i in range(0,10):
-#     if i == 0:
-#         mean_rewards.append(pendulum.train_agent(tr_epsds, epsd_steps))
-#     else:
-#         mean_rewards.append(pendulum.train_agent(tr_epsds, epsd_steps, initialization=False))
-
-# import time
-# import cProfile
-# from system import System
-
-# def main():
-#     hopper = System()
-#     tr_epsds = 50
-#     epsd_steps = 800
-#     mean_rewards = []
-#     mean_rewards.append(hopper.train_agent(tr_epsds, epsd_steps))
-#     # for i in range(0,1):
-#     #     if i == 0:
-#     #         mean_rewards.append(hopper.train_agent(tr_epsds, epsd_steps))
-#     #     else:
-#     #         mean_rewards.append(hopper.train_agent(tr_epsds, epsd_steps, initialization=False))
-
-#     import pickle
-#     pickle.dump(hopper,open('hopper_3.p','wb'))
-#     import numpy as np
-#     np.savetxt('mean_rewards_3".txt',mean_rewards)
-
-# if __name__ == '__main__':
-#     main()
-#     # cProfile.run('main()')
-
 import pickle
 import numpy as np
 from system import System
